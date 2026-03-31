e46527b8a6eb173c530f9c829eee163de75bbc84
rayne choy <echoy5@my.bcit.ca>
2026-03-31 12:25:02 -0700

    populate event detail page with proper values

--- src/eventDetails.js
+++ src/eventDetails.js
@@ -1,9 +1,16 @@
 import { renderEvents } from "./components/EventsRow.js";
-import { queryEvents, getNumAttendees, getEvent, getVenueID } from "./events";
+import {
+  queryEvents,
+  getNumAttendees,
+  getEvent,
+  getVenueID,
+  getVenue,
+} from "./events";
 import { db } from "/src/firebaseConfig.js";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { arrayUnion } from "firebase/firestore";
 import { onAuthReady } from "./authentication.js";
+import { svgs } from "../src/svgs.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   onAuthReady(async (user) => {
@@ -22,7 +29,16 @@ function initEventDetails(user) {
   const joinBtn = document.getElementById("join");
   const eventId = getEventIdFromUrl();
 
-  const nameEl = document.getElementById("event-name");
+  //element refs
+  const eventVsEl = document.getElementById("team-vs");
+  const team1Image = document.getElementById("team-1-image");
+  const team2Image = document.getElementById("team-2-image");
+  const eventDesc = document.getElementById("event-description");
+  const venueDesc = document.getElementById("venue-description");
+  const venueType = document.getElementById("venue-type");
+  const venuePhone = document.getElementById("venue-phone");
+  const venueWeb = document.getElementById("venue-website");
+  const venueInsta = document.getElementById("venue-instagram");
 
   function showError(message) {
     if (!alertEl) return;
@@ -37,28 +53,67 @@ function initEventDetails(user) {
     }
 
     try {
+      //refs
       const eventRef = doc(db, "events", eventId);
       const eventSnap = await getDoc(eventRef);
+      const currentVenueId = await getVenueID(eventId);
+      const venueRef = doc(db, "business_accounts", currentVenueId);
+      const venueSnap = await getDoc(venueRef);
 
-      if (!eventSnap.exists()) {
+      if (!eventSnap.exists() || !venueSnap.exists()) {
         showError("Event not found.");
         return;
       }
 
+      //data
       const event = eventSnap.data();
-
+      const venue = venueSnap.data();
+      const venueLoc = `${venue.address}, ${venue.city}, ${venue.province}, ${venue.postalCode}`;
       const attendeesCount = await getNumAttendees(eventId);
 
-      if (nameEl) {
-        nameEl.textContent = event.name || "Unnamed Event";
-      }
+      //assign values
+      if (eventVsEl) eventVsEl.textContent = event.team1 + " vs " + event.team2;
+      if (team1Image) team1Image.src = `../../images/flags/${event.team1}.png`;
+      if (team2Image) team2Image.src = `../../images/flags/${event.team2}.png`;
+      if (eventDesc) eventDesc.textContent = event.description || "";
 
+      if (venueDesc) venueDesc.textContent = venue.description || "";
+      if (venueType && venue.businessType)
+        venueType.innerHTML =
+          venueInfoTemplate(
+            venue.businessType,
+            svgs.beer(25, 25, "currentColor"),
+          ) || "";
+      if (venuePhone && venue.phone)
+        venuePhone.innerHTML =
+          venueInfoTemplate(venue.phone, svgs.phone(25, 25, "currentColor")) ||
+          "";
+      if (venueWeb && venue.website)
+        venueWeb.innerHTML =
+          venueInfoTemplate(
+            venue.website,
+            svgs.website(25, 25, "currentColor"),
+          ) || "";
+      if (venueInsta && venue.instagram)
+        venueInsta.innerHTML =
+          venueInfoTemplate(
+            venue.instagram,
+            svgs.instagram(25, 25, "currentColor"),
+          ) || "";
+
+      document.querySelectorAll(".event-name").forEach((el) => {
+        el.textContent = event.name || "Unnamed Event";
+      });
       document.querySelectorAll(".date").forEach((el) => {
-        el.textContent = event.date || "N/A";
+        el.textContent = event.date || "TBD";
       });
 
       document.querySelectorAll(".time").forEach((el) => {
-        el.textContent = event.time || "N/A";
+        el.textContent = event.time || "TBD";
+      });
+
+      document.querySelectorAll(".location").forEach((el) => {
+        el.textContent = venueLoc || "TBD";
       });
 
       document.querySelectorAll(".attendees").forEach((el) => {
@@ -103,6 +158,13 @@ function initEventDetails(user) {
   });
 
   loadEvent();
+}
+
+function venueInfoTemplate(value, svg) {
+  return `<div class="flex gap-2">
+            ${svg}
+            <p class="subtitle font-semibold">${value}</p>
+          </div>`;
 }
 
 async function getOtherEvents() {
